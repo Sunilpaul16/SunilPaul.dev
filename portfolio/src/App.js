2fba91b290d1675e67bfd3719b0a198b51122114
Sunil <sunilpaul16@gmail.com>
2024-11-12 03:52:20 +0000

    Refactor App component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './Components/About/About';
 import './App.css';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import Nav from './Components/NavBar/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React kkk
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={
+            <>
+            <Nav />
+              <About />
+              <Contact />
+              <Footer />
+            </>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

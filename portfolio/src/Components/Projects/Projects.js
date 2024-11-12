65933fd9594a2d5ae358c749ae464af4b14716b8
Sunil <sunilpaul16@gmail.com>
2024-11-12 04:30:17 +0000

    Add LeftSocialMediaIcons

---
+++ portfolio/src/Components/Projects/Projects.js
@@ -0,0 +1,11 @@
+import Project1 from "./Project1";
+
+function Projects() {
+  return (
+    <section className="projects">
+      <Project1 id="project1" />
+    </section>
+  );
+}
+
+export default Projects;

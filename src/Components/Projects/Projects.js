9ad0da23c6de62799b6d591cb7f81a02b5921b43
Sunil <sunilpaul16@gmail.com>
2024-11-14 17:23:23 +0000

    Replace Project1

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,8 +4,8 @@ function Projects() {
   return (
     <section className="projects">
       <Project1 id="project1" />
-      <Project1 id="project2" />
-      <Project1 id="project3" />
+      <Project2 />
+      <Project3 />
     </section>
   );
 }

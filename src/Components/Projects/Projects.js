5db4b16b566951976d77c1dca62fc418fcf8bf7a
Sunil <sunilpaul16@gmail.com>
2024-11-15 21:23:23 +0000

    Add additional Project1 components for project2 and project3

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -4,6 +4,8 @@ function Projects() {
   return (
     <section className="projects">
       <Project1 id="project1" />
+      <Project1 id="project2" />
+      <Project1 id="project3" />
     </section>
   );
 }
